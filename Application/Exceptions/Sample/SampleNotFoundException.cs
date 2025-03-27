a5b72fbcc4acc8a92f8982a757c9ec39d8fc0fa9
brayan henao <ing01fs@btw.com.co>
2025-03-27 00:09:58 -0500

    feat: add shared files

--- Application/Exceptions/Sample/SampleNotFoundException.cs
+++ Application/Exceptions/Sample/SampleNotFoundException.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Net;
+using Shared.Resources;
 
 namespace Application.Exceptions.Sample;
 
-public class SampleNotFoundException(int id) : BaseException($"No se encontró un Sample con el id {id}", HttpStatusCode.NotFound)
+public class SampleNotFoundException(int id) : BaseException(string.Format(Messages.SampleNotFound, id), HttpStatusCode.NotFound)
 {
 }
